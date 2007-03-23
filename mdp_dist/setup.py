cc4853d40779a3c6c523986c842388f7bfd8c630
Tiziano Zito <tiziano.zito@bccn-berlin.de>
2007-03-23 19:06:34 +0000

    release commit
    git-svn-id: https://mdp-toolkit.svn.sourceforge.net/svnroot/mdp-toolkit/mdp/trunk@162 c860887f-54fc-0310-8c92-9a9c51041e24

--- mdp_dist/setup.py
+++ mdp_dist/setup.py
@@ -66,7 +66,7 @@ classifiers = ["Development Status :: 5 - Production/Stable",
                "Topic :: Scientific/Engineering :: Mathematics",
                "Topic :: Software Development :: Algorithms"]
 
-setup(name = 'MDP', version = '2.0RC',
+setup(name = 'MDP', version = '2.1',
       author = 'Pietro Berkes and Tiziano Zito',
       author_email = 'berkes@gatsby.ucl.ac.uk, t.zito@biologie.hu-berlin.de',
       maintainer = 'Pietro Berkes and Tiziano Zito',
