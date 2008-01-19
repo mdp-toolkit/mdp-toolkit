2785e7be13651abda1de134350abc3ce2be6f3ab
Yaroslav Halchenko <debian@onerussian.com>
2008-01-19 16:52:22 -0500

    dh_make templates + adjusted control

---
+++ debian/mdp-default.ex
@@ -0,0 +1,10 @@
+# Defaults for mdp initscript
+# sourced by /etc/init.d/mdp
+# installed at /etc/default/mdp by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(mdp):needs="X11|text|vc|wm" section="Apps/see-menu-manual"\
+  title="mdp" command="/usr/bin/mdp"
