98ddceed4c3e64ae86d71de1a02c47118866ad2c
Yaroslav Halchenko <debian@onerussian.com>
2008-01-20 00:23:29 -0500

    Removed some leftovers. Ignore .pyc files

--- debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the mdp package
-#
-0 4	* * *	root	mdp_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(mdp):needs="X11|text|vc|wm" section="Apps/see-menu-manual"\
-  title="mdp" command="/usr/bin/mdp"
