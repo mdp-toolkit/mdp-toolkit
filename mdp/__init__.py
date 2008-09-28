0d5a7456f1428a7c8f1f5dd1396fb39bc920fc04
Tiziano Zito <tiziano.zito@bccn-berlin.de>
2008-09-28 06:52:13 +0000

    fixed 'reload' in ipython.
    git-svn-id: https://mdp-toolkit.svn.sourceforge.net/svnroot/mdp-toolkit/mdp/trunk@314 c860887f-54fc-0310-8c92-9a9c51041e24

--- mdp/__init__.py
+++ mdp/__init__.py
@@ -136,8 +136,11 @@ import hinet
 from test import test
 
 # clean up namespace
-del signal_node
-del linear_flows
+# trick to enable ipython reload
+if 'signal_node' in locals():
+    del signal_node
+if 'linear_flows' in locals():
+    del linear_flows
 
 # explicitly set __all__, mainly needed for epydoc
 __all__ = ['CheckpointFlow', 'CheckpointFunction', 'CheckpointSaveFunction',
