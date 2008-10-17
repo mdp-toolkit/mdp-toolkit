8b789a737e55c72e46924a82f1be644c29985d7c
Pietro Berkes <pietro.berkes@googlemail.com>
2008-10-17 09:58:13 +0000

    remove pylab from node
    git-svn-id: https://mdp-toolkit.svn.sourceforge.net/svnroot/mdp-toolkit/mdp/trunk@346 c860887f-54fc-0310-8c92-9a9c51041e24

--- mdp/contrib/__init__.py
+++ mdp/contrib/__init__.py
@@ -1,9 +1,9 @@
 from jade import JADENode
 from nipals import NIPALSNode
-from lle_nodes import LLENode
+from lle_nodes import LLENode, HLLENode
 
 #del jade
 #del nipals
 #del lle_nodes
 
-__all__ = ['JADENode', 'NIPALSNode', 'LLENode']
+__all__ = ['JADENode', 'NIPALSNode', 'LLENode', 'HLLENode']
