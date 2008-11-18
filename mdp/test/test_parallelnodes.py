879b1a1a1621056dded4f4372d24b5f0987c6ed7
Niko Wilbert <mail@nikowilbert.de>
2008-11-18 13:21:18 +0000

    Added ParallelFDANode and corresponding unittest.
    git-svn-id: https://mdp-toolkit.svn.sourceforge.net/svnroot/mdp-toolkit/mdp/trunk@414 c860887f-54fc-0310-8c92-9a9c51041e24

--- mdp/test/test_parallelnodes.py
+++ mdp/test/test_parallelnodes.py
@@ -3,9 +3,8 @@ import unittest
 
 import mdp
 import mdp.parallel as parallel
-from mdp import numx as n
-# TODO: replace this with the MDP testing tools
-import numpy.testing as testing_tools
+from mdp import utils, numx, numx_rand
+from testing_tools import assert_array_almost_equal, assert_almost_equal
 
 
 class TestParallelMDPNodes(unittest.TestCase):
@@ -13,8 +12,8 @@ class TestParallelMDPNodes(unittest.TestCase):
     def test_PCANode(self):
         """Test Parallel PCANode"""
         precision = 6
-        x = n.random.random([100,10])
-        x_test = n.random.random([20,10])
+        x = numx_rand.random([100,10])
+        x_test = numx_rand.random([20,10])
         pca_node = mdp.nodes.PCANode()
         parallel_pca_node = parallel.ParallelPCANode()
         chunksize = 25
@@ -25,21 +24,20 @@ class TestParallelMDPNodes(unittest.TestCase):
             forked_node = parallel_pca_node.fork()
             forked_node.train(chunk)
             parallel_pca_node.join(forked_node)
-        testing_tools.assert_array_almost_equal(
-                                        pca_node._cov_mtx._cov_mtx, 
-                                        parallel_pca_node._cov_mtx._cov_mtx, 
-                                        precision)
+        assert_array_almost_equal(pca_node._cov_mtx._cov_mtx, 
+                                  parallel_pca_node._cov_mtx._cov_mtx, 
+                                  precision)
         pca_node.stop_training()
         y1 = pca_node.execute(x_test)
         parallel_pca_node.stop_training()
         y2 = parallel_pca_node.execute(x_test)
-        testing_tools.assert_array_almost_equal(y1, y2, precision)
+        assert_array_almost_equal(y1, y2, precision)
         
     def test_WhiteningNode(self):
         """Test Parallel WhiteningNode"""
         precision = 6
-        x = n.random.random([100,10])
-        x_test = n.random.random([20,10])
+        x =numx_rand.random([100,10])
+        x_test = numx_rand.random([20,10])
         pca_node = mdp.nodes.WhiteningNode()
         parallel_pca_node = parallel.ParallelWhiteningNode()
         chunksize = 25
@@ -50,21 +48,20 @@ class TestParallelMDPNodes(unittest.TestCase):
             forked_node = parallel_pca_node.fork()
             forked_node.train(chunk)
             parallel_pca_node.join(forked_node)
-        testing_tools.assert_array_almost_equal(
-                                        pca_node._cov_mtx._cov_mtx, 
-                                        parallel_pca_node._cov_mtx._cov_mtx, 
-                                        precision)
+        assert_array_almost_equal(pca_node._cov_mtx._cov_mtx, 
+                                  parallel_pca_node._cov_mtx._cov_mtx, 
+                                  precision)
         pca_node.stop_training()
         y1 = pca_node.execute(x_test)
         parallel_pca_node.stop_training()
         y2 = parallel_pca_node.execute(x_test)
-        testing_tools.assert_array_almost_equal(y1, y2, precision)
+        assert_array_almost_equal(y1, y2, precision)
 
     def test_SFANode(self):
         """Test Parallel SFANode"""
         precision = 6
-        x = n.random.random([100,10])
-        x_test = n.random.random([20,10])
+        x = numx_rand.random([100,10])
+        x_test = numx_rand.random([20,10])
         sfa_node = mdp.nodes.SFANode()
         parallel_sfa_node = parallel.ParallelSFANode()
         chunksize = 25
@@ -75,21 +72,20 @@ class TestParallelMDPNodes(unittest.TestCase):
             forked_node = parallel_sfa_node.fork()
             forked_node.train(chunk)
             parallel_sfa_node.join(forked_node)
-        testing_tools.assert_array_almost_equal(
-                                        sfa_node._cov_mtx._cov_mtx, 
-                                        parallel_sfa_node._cov_mtx._cov_mtx, 
-                                        precision)
+        assert_array_almost_equal(sfa_node._cov_mtx._cov_mtx, 
+                                  parallel_sfa_node._cov_mtx._cov_mtx, 
+                                  precision)
         sfa_node.stop_training()
         y1 = sfa_node.execute(x_test)
         parallel_sfa_node.stop_training()
         y2 = parallel_sfa_node.execute(x_test)
-        testing_tools.assert_array_almost_equal(y1, y2, precision)
+        assert_array_almost_equal(y1, y2, precision)
         
     def test_SFA2Node(self):
         """Test Parallel SFA2Node"""
         precision = 6
-        x = n.random.random([100,10])
-        x_test = n.random.random([20,10])
+        x = numx_rand.random([100,10])
+        x_test = numx_rand.random([20,10])
         sfa2_node = mdp.nodes.SFA2Node()
         parallel_sfa2_node = parallel.ParallelSFA2Node()
         chunksize = 25
@@ -100,15 +96,53 @@ class TestParallelMDPNodes(unittest.TestCase):
             forked_node = parallel_sfa2_node.fork()
             forked_node.train(chunk)
             parallel_sfa2_node.join(forked_node)
-        testing_tools.assert_array_almost_equal(
-                                        sfa2_node._cov_mtx._cov_mtx, 
-                                        parallel_sfa2_node._cov_mtx._cov_mtx, 
-                                        precision)
+        assert_array_almost_equal(sfa2_node._cov_mtx._cov_mtx, 
+                                  parallel_sfa2_node._cov_mtx._cov_mtx, 
+                                  precision)
         sfa2_node.stop_training()
         y1 = sfa2_node.execute(x_test)
         parallel_sfa2_node.stop_training()
         y2 = parallel_sfa2_node.execute(x_test)
-        testing_tools.assert_array_almost_equal(y1, y2, precision)
+        assert_array_almost_equal(y1, y2, precision)
+        
+    def test_FDANode(self):
+        """Test Parallel FDANode."""
+        # this test code is an adaption of the FDANode test
+        precision = 4
+        mean1 = [0., 2.]
+        mean2 = [0., -2.]
+        std_ = numx.array([1., 0.2])
+        npoints = 50000
+        rot = 45
+        # input data: two distinct gaussians rotated by 45 deg
+        def distr(size): 
+            return numx_rand.normal(0, 1., size=(size)) * std_
+        x1 = distr((npoints,2)) + mean1
+        utils.rotate(x1, rot, units='degrees')
+        x2 = distr((npoints,2)) + mean2
+        utils.rotate(x2, rot, units='degrees')
+        # labels
+        cl1 = numx.ones((x1.shape[0],), dtype='d')
+        cl2 = 2.*numx.ones((x2.shape[0],), dtype='d')
+        flow = parallel.ParallelFlow([parallel.ParallelFDANode()])
+        flow.train([[(x1, cl1), (x2, cl2)]], scheduler=parallel.Scheduler())
+        fda_node = flow[0]
+        assert fda_node.tlens[1] == npoints
+        assert fda_node.tlens[2] == npoints
+        m1 = numx.array([mean1])
+        m2 = numx.array([mean2])
+        utils.rotate(m1, rot, units='degrees')
+        utils.rotate(m2, rot, units='degrees')
+        assert_array_almost_equal(fda_node.means[1], m1, 2)
+        assert_array_almost_equal(fda_node.means[2], m2, 2)
+        y = flow.execute([x1, x2], scheduler=parallel.Scheduler())
+        assert_array_almost_equal(numx.mean(y, axis=0), [0., 0.], precision)
+        assert_array_almost_equal(numx.std(y, axis=0), [1., 1.], precision)
+        assert_almost_equal(utils.mult(y[:,0], y[:,1].T), 0., precision)
+        v1 = fda_node.v[:,0]/fda_node.v[0,0]
+        assert_array_almost_equal(v1, [1., -1.], 2)
+        v1 = fda_node.v[:,1]/fda_node.v[0,1]
+        assert_array_almost_equal(v1, [1., 1.], 2)
         
 
 def get_suite(testname=None):
