c292633396627e5c23510cad92aa11db702df2cd
Tiziano Zito <tiziano.zito@bccn-berlin.de>
2009-06-16 09:39:28 +0000

    - updated list of contributors in tutorial.
    git-svn-id: https://mdp-toolkit.svn.sourceforge.net/svnroot/mdp-toolkit/mdp/trunk@558 c860887f-54fc-0310-8c92-9a9c51041e24

--- mdp/contrib/xsfa_nodes.py
+++ mdp/contrib/xsfa_nodes.py
@@ -42,6 +42,12 @@ class XSFANode(mdp.Node):
     An Extension of Slow Feature Analysis for Nonlinear Blind Source Separation
     Journal of Machine Learning Research, submitted
     [pdf link follows]
+
+    Original code contributed by:
+      Henning Sprekeler
+      Laboratory of Computational Neuroscience Ecole Polytechnique Federal de
+      Lausanne (EPFL), Switzerland
+      henning.sprekeler@epfl.ch
     """
     def __init__(self, basic_exp=None, intern_exp=None, svd=False, verbose=False,
                  input_dim=None, output_dim=None, dtype=None):
