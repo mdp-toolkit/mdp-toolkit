c372cdb6eb7540a354589d7cfccfdbbf91fe3f6e
Tiziano Zito <tiziano.zito@bccn-berlin.de>
2009-06-30 09:28:07 +0000

    updatred copyright date
    git-svn-id: https://mdp-toolkit.svn.sourceforge.net/svnroot/mdp-toolkit/mdp/trunk@570 c860887f-54fc-0310-8c92-9a9c51041e24

--- mdp/__init__.py
+++ mdp/__init__.py
@@ -154,6 +154,6 @@ __all__ = ['CheckpointFlow', 'CheckpointFunction', 'CheckpointSaveFunction',
 __version__ = '2.5'
 __revision__ = utils.get_svn_revision()
 __authors__ = 'Pietro Berkes, Niko Wilbert, and Tiziano Zito'
-__copyright__ = '(c) 2003-2008 Pietro Berkes, Niko Wilbert, Tiziano Zito'
+__copyright__ = '(c) 2003-2009 Pietro Berkes, Niko Wilbert, Tiziano Zito'
 __license__ = 'LGPL v3, http://www.gnu.org/licenses/lgpl.html'
 __contact__ = 'mdp-toolkit-users AT lists.sourceforge.net'
