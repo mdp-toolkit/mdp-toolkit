b8e4d18107fdd35ef372c53e34bd9b161ae1034a
Niko Wilbert <mail@nikowilbert.de>
2009-11-06 14:24:30 +0000

    Rewrote some parts of the extension mechanism. It now works for arbitrary attributes. One nice things is that this actually reduced the number of lines of code. The tutorial has not yet been updated. Attribute specific unittests are missing as well.
    git-svn-id: https://mdp-toolkit.svn.sourceforge.net/svnroot/mdp-toolkit/mdp/trunk@697 c860887f-54fc-0310-8c92-9a9c51041e24

--- mdp/extension.py
+++ mdp/extension.py
@@ -24,17 +24,10 @@ Note that static methods and class methods are also supported by the
 extension mechanism.
 """
 
-import types
-
 from mdp import MDPException, NodeMetaclass
 
-# TODO: Add support data-attributes attributes (like classmethods).
-#    store ext_original_method in _extensions?
-#    can drop ext_extension_name
-#    have to change the activation and deactivation
-#    see http://stackoverflow.com/questions/1677468/how-does-a-classmethod-object-work
-#    for classmethod issue
-#    add unittests for classmethods, staticmethods and data attributes
+# TODO: update the Tutorial
+# TODO: add attribute specific unittests (e.g. for classmethods)
 
 # Note: There is no way to get the affected node instances when an extension
 #    is activated, these are not registered anywhere.
@@ -48,11 +41,18 @@ from mdp import MDPException, NodeMetaclass
 # TODO: in the future could use ABC's to register nodes with extension nodes
 
 
+# name prefix used for the original attributes when they are shadowed
+ORIGINAL_ATTR_PREFIX = "_non_extension_"
+# prefix used to store the current extension name for an attribute 
+EXTENSION_ATTR_PREFIX = "_extension_for_"
+# list of attribute names that are not affected by extensions,
+NON_EXTENSION_ATTRIBUTES = ["__module__", " __doc__", "extension_name"]
+
 # dict of dicts of dicts, contains a key for each extension,
 # the inner dict maps the node types to their extension node,
-# the innermost dict then maps method names to functions
+# the innermost dict then maps attribute names to values
+# (e.g. a method name to the actual function)
 _extensions = dict()
-
 # set containing the names of the currently activated extensions
 _active_extensions = set()
 
@@ -62,26 +62,13 @@ class ExtensionException(MDPException):
     pass
 
 
-def _register_function(ext_name, node_cls, func, method_name=None):
-    """Register a function as an extension method.
+def _register_attribute(ext_name, node_cls, attr_name, attr_value):
+    """Register an attribute as an extension attribute.
     
     ext_name -- String with the name of the extension.
     node_cls -- Node class for which the method should be registered.
-    func -- Function to be registered as an extension method.
     """
-    if not method_name:
-        method_name = func.__name__
-    # perform safety check
-    if method_name in node_cls.__dict__:
-        if not isinstance(getattr(node_cls, method_name), types.MethodType):
-            err = ("Extension method " + method_name + " tries to "
-                   "override non-method attribute in class " +
-                   str(node_cls))
-            raise ExtensionException(err)
-    _extensions[ext_name][node_cls][method_name] = func
-    # do not set this now to be more flexibel
-    func.ext_original_method = None
-    func.ext_extension_name = ext_name
+    _extensions[ext_name][node_cls][attr_name] = attr_value
     
 def extension_method(ext_name, node_cls, method_name=None):
     """Returns a function to register a function as extension method.
@@ -98,6 +85,9 @@ def extension_method(ext_name, node_cls, method_name=None):
     (the function will be called as a method of the node class).
     """
     def register_function(func):
+        _method_name = method_name
+        if not _method_name:
+            _method_name = func.__name__
         if not ext_name in _extensions:
             err = ("No ExtensionNode base class has been defined for this "
                    "extension.")
@@ -105,7 +95,7 @@ def extension_method(ext_name, node_cls, method_name=None):
         if not node_cls in _extensions[ext_name]:
             # register this node
             _extensions[ext_name][node_cls] = dict()
-        _register_function(ext_name, node_cls, func, method_name)
+        _register_attribute(ext_name, node_cls, _method_name, func)
         return func
     return register_function
 
@@ -163,12 +153,10 @@ class ExtensionNodeMetaclass(NodeMetaclass):
             # register the base node
             _extensions[ext_name][base_node_cls] = dict()
         # register methods
-        for member in members.values():
-            # TODO: change this to accept attributes
-            if (isinstance(member, types.FunctionType) or
-                isinstance(member, classmethod) or
-                isinstance(member, staticmethod)):
-                _register_function(ext_name, base_node_cls, member)
+        for attr_name, attr_value in members.items():
+            if attr_name not in NON_EXTENSION_ATTRIBUTES:
+                _register_attribute(ext_name, base_node_cls,
+                                    attr_name, attr_value)
         return ext_node_cls
                                                      
 
@@ -211,37 +199,41 @@ def get_extensions():
     return _extensions
 
 def get_active_extensions():
-    """Returns the set with the names of the currently activated extensions."""
+    """Returns a list with the names of the currently activated extensions."""
     # use copy to protect the original set, also important if the return
     # value is used in a for-loop (see deactivate_extensions function)
-    return _active_extensions.copy()
+    return list(_active_extensions)
     
 def activate_extension(extension_name):
     """Activate the extension by injecting the extension methods."""
+    if extension_name not in _extensions.keys():
+        err = "Unknown extension name: %s" + str(extension_name)
+        raise ExtensionException(err)
     if extension_name in _active_extensions:
         return
     _active_extensions.add(extension_name)
     try:
-        for node_cls, methods in _extensions[extension_name].items():
-            for method_name, method in methods.items():
-                if method_name in node_cls.__dict__:
-                    original_method = getattr(node_cls, method_name)
-                    ## perform safety checks
-                    # same check as in _register_function
-                    if not isinstance(original_method, types.MethodType):
-                        err = ("Extension method " + method_name + " tries to "
-                               "override non-method attribute in class " +
-                               str(node_cls))
-                        raise ExtensionException(err)
-                    if hasattr(original_method, "ext_extension_name"):
-                        err = ("Method name overlap for method '" + 
-                               method_name + "' between extension '" +
-                               getattr(original_method, "ext_extension_name")
+        for node_cls, attributes in _extensions[extension_name].items():
+            for attr_name, attr_value in attributes.items():
+                if attr_name in node_cls.__dict__:
+                    if ((EXTENSION_ATTR_PREFIX + attr_name) in
+                        node_cls.__dict__):
+                        # two extensions override the same attribute
+                        err = ("Name collision for attribute '" + 
+                               attr_name + "' between extension '" +
+                               getattr(node_cls,
+                                       EXTENSION_ATTR_PREFIX + attr_name)
                                + "' and newly activated extension '" +
                                extension_name + "'.")
                         raise ExtensionException(err)
-                    method.ext_original_method = original_method
-                setattr(node_cls, method_name, method)
+                    original_attr = getattr(node_cls, attr_name)
+                    setattr(node_cls, ORIGINAL_ATTR_PREFIX + attr_name,
+                            original_attr)
+                setattr(node_cls, attr_name, attr_value)
+                # store to which extension this attribute belongs, this is also
+                # used as a flag that this is an extension attribute
+                setattr(node_cls, EXTENSION_ATTR_PREFIX + attr_name,
+                        extension_name)
     except:
         # make sure that an incomplete activation is reverted
         deactivate_extension(extension_name)
@@ -249,25 +241,38 @@ def activate_extension(extension_name):
 
 def deactivate_extension(extension_name):
     """Deactivate the extension by removing the injected methods."""
+    if extension_name not in _extensions.keys():
+        err = "Unknown extension name: " + str(extension_name)
+        raise ExtensionException(err)
     if extension_name not in _active_extensions:
         return
-    for node_cls, methods in _extensions[extension_name].items():
-        for method_name, method in methods.items():
-            if method.ext_original_method is not None:
-                original_method = method.ext_original_method
-                setattr(node_cls, method_name, original_method)
-                method.ext_original_method = None
+    for node_cls, attributes in _extensions[extension_name].items():
+        for attr_name in attributes.keys():
+            original_name = ORIGINAL_ATTR_PREFIX + attr_name
+            if original_name in node_cls.__dict__:
+                # restore the original attribute
+                original_attr = getattr(node_cls, original_name)
+                setattr(node_cls, attr_name, original_attr)
+                delattr(node_cls, original_name)
             else:
-                # if the activation process failed then the extension method
-                # might be missing, so be tolerant
                 try:
-                    delattr(node_cls, method_name)
+                    # no original attribute to restore, so simply delete
+                    # might be missing if the activation failed
+                    delattr(node_cls, attr_name)
                 except AttributeError:
                     pass
+            try:
+                # might be missing if the activation failed
+                delattr(node_cls, EXTENSION_ATTR_PREFIX + attr_name)
+            except AttributeError:
+                pass
     _active_extensions.remove(extension_name)
 
-def activate_extensions(*extension_names):
-    """Activate all the extensions for the given names."""
+def activate_extensions(extension_names):
+    """Activate all the extensions for the given names.
+    
+    extension_names -- Sequence of extension names.
+    """
     try:
         for extension_name in extension_names:
             activate_extension(extension_name)
@@ -278,7 +283,7 @@ def activate_extensions(*extension_names):
         deactivate_extensions(get_active_extensions())
         raise
 
-def deactivate_extensions(*extension_names):
+def deactivate_extensions(extension_names):
     """Deactivate all the extensions for the given names.
     
     extension_names -- Sequence of extension names.
