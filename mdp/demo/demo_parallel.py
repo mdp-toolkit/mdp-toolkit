48b8d14267fa84e9f6d149dad7ed10b724ad526b
Niko Wilbert <mail@nikowilbert.de>
2009-12-09 15:07:40 +0000

    Added a small parallelization demo to demonstrate and explore the speedup.
    git-svn-id: https://mdp-toolkit.svn.sourceforge.net/svnroot/mdp-toolkit/mdp/trunk@734 c860887f-54fc-0310-8c92-9a9c51041e24

---
+++ mdp/demo/demo_parallel.py
@@ -0,0 +1,56 @@
+"""
+Demo for the speedup that the parallelization can offer.
+
+The parallelization usually pays off when there is lots of data (so that
+stop_training is fast compared to the train calls) and when the processing
+of the data chunks is costly enough to dominate over the cost of sending the
+training data to the parallel worker processes (e.g. if a non-linear exapansion
+is involved then this increases the training time without increasing the
+data volume).
+
+With the current example values the parallelized version is faster by a factor
+of 2.2 on an Intel Core 2 Quad @ 2.66 GHz. The parallel training time is
+33 seconds. While this factor is much smaller than the theoretical maximum of 4
+it can be increased by using more data chunks and a larger input dimension.
+For training times over 10 minutes one can thus achieve a speedup factor > 3.
+
+Parallelization only pays off for large training tasks, which is actually not
+that bad since this is where the speedup is most needed.
+"""
+
+import numpy as np
+import mdp
+import time
+
+n_processes = 4
+
+n_chunks = 30
+n_chunk_samples = 10000
+n_dim = 30
+
+## parallel training
+
+print "starting parallel training..."
+flow = mdp.parallel.ParallelFlow([mdp.nodes.SFA2Node()])
+x_iter = [np.random.random((n_chunk_samples, n_dim)) for _ in range(n_chunks)]
+scheduler = mdp.parallel.ProcessScheduler(n_processes=n_processes,
+                                          verbose=True)
+
+start_time = time.time()
+flow.train([x_iter], scheduler)
+parallel_time = time.time() - start_time
+scheduler.shutdown()
+print "parallel in %.3f secs" % parallel_time
+
+## sequential training
+
+print "starting sequential training..."
+flow = mdp.Flow([mdp.nodes.SFA2Node()])
+
+start_time = time.time()
+flow.train([x_iter])
+nonparallel_time = time.time() - start_time
+print "sequential in %.3f secs" % nonparallel_time
+
+speedup = 1.0 * nonparallel_time / parallel_time
+print "speedup factor: %.1f (%d processes)" % (speedup, n_processes)
