c046cec40adac1f08848060d74f817f48b1ab3b6
Niko Wilbert <mail@nikowilbert.de>
2010-02-11 17:29:36 +0000

    Updated the parallel demo.
    git-svn-id: https://mdp-toolkit.svn.sourceforge.net/svnroot/mdp-toolkit/mdp/trunk@781 c860887f-54fc-0310-8c92-9a9c51041e24

--- mdp/demo/demo_parallel.py
+++ mdp/demo/demo_parallel.py
@@ -1,45 +1,50 @@
 """
 Demo for the speedup that the parallelization can offer.
 
-The parallelization usually pays off when there is lots of data (so that
-stop_training is fast compared to the train calls) and when the processing
+This demo compares the thread and the process based paralelisation with the
+non-parallel version.
+
+The process based-parallelization only pays off when there is lots of data (so
+that stop_training is fast compared to the train calls) and when the processing
 of the data chunks is costly enough to dominate over the cost of sending the
 training data to the parallel worker processes (e.g. if a non-linear exapansion
-is involved then this increases the training time without increasing the
-data volume).
+is involved then this increases the training time without increasing the data
+volume).
 
-With the current example values the parallelized version is faster by a factor
-of 2.2 on an Intel Core 2 Quad @ 2.66 GHz. The parallel training time is
-33 seconds. While this factor is much smaller than the theoretical maximum of 4
-it can be increased by using more data chunks and a larger input dimension.
-For training times over 10 minutes one can thus achieve a speedup factor > 3.
+In principle the process-based parallelisation can beat the thread-based one if
+the GIL prevents the threads from using the cores effectively.
 
-Parallelization only pays off for large training tasks, which is actually not
-that bad since this is where the speedup is most needed.
+The thread-based parallelisation carries some overhead over the non-parallel
+version because it has to fork the nodes and creates threads.
 """
 
 import numpy as np
 import mdp
 import time
 
+n_threads = 2
 n_processes = 2
 
-n_chunks = 10
-n_chunk_samples = 5000
+n_chunks = 20
+n_chunk_samples = 1000
 n_dim = 30
+
+nonparallel_time = None
+thread_time = None
+process_time = None
 
 ## threads
 
 print "starting thread parallel training..."
 flow = mdp.parallel.ParallelFlow([mdp.nodes.SFA2Node()])
 x_iter = [np.random.random((n_chunk_samples, n_dim)) for _ in range(n_chunks)]
-scheduler = mdp.parallel.ThreadScheduler(n_threads=n_processes, verbose=True)
+scheduler = mdp.parallel.ThreadScheduler(n_threads=n_threads, verbose=True)
 
 start_time = time.time()
 flow.train([x_iter], scheduler)
-parallel_time = time.time() - start_time
+thread_time = time.time() - start_time
 scheduler.shutdown()
-print "thread parallel in %.3f secs" % parallel_time
+print "thread parallel in %.3f secs" % thread_time
 
 ## processes
 
@@ -50,9 +55,9 @@ scheduler = mdp.parallel.ProcessScheduler(n_processes=n_processes, verbose=True)
 
 start_time = time.time()
 flow.train([x_iter], scheduler)
-parallel_time = time.time() - start_time
+process_time = time.time() - start_time
 scheduler.shutdown()
-print "process parallel in %.3f secs" % parallel_time
+print "process parallel in %.3f secs" % process_time
 
 ## sequential training
 
@@ -64,5 +69,9 @@ flow.train([x_iter])
 nonparallel_time = time.time() - start_time
 print "sequential in %.3f secs" % nonparallel_time
 
-speedup = 1.0 * nonparallel_time / parallel_time
-print "speedup factor: %.1f (%d processes)" % (speedup, n_processes)
+if nonparallel_time and thread_time:
+    speedup = 1.0 * nonparallel_time / thread_time
+    print "thread speedup factor: %.1f (%d threads)" % (speedup, n_threads)
+if nonparallel_time and process_time:
+    speedup = 1.0 * nonparallel_time / process_time
+    print "process speedup factor: %.1f (%d processes)" % (speedup, n_processes)
