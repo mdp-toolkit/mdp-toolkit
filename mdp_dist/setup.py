6cc3f01d43b62bfa20d2c529d9781419819607e1
Tiziano Zito <opossumnano@gmail.com>
2010-03-04 18:30:55 +0100

    Revert "Delete mdp_dist. All future updates should be made in release-manager branch."
    This reverts commit ba2743e19d6edd5df46427c3abf1304d7f259b76.
    The idea of a release-manager branch doesn't actually pay off, because
    all the files in mdp_dist should actually be in remote master branch
    apart from the two scripts (gendist & cleandist). I think the two
    scripts can live in the master branch: this would even help the wannabe
    mdp-forker ;-)

---
+++ mdp_dist/setup.py
@@ -0,0 +1,87 @@
+from distutils.core import setup
+
+short_description = "Modular toolkit for Data Processing (MDP) is a library of widely used data processing algorithms that can be combined according to a pipeline analogy to build more complex data processing software. Implemented algorithms include Principal Component Analysis (PCA), Independent Component Analysis (ICA), Slow Feature Analysis (SFA), and many more."
+
+long_description = """
+The Modular toolkit for Data Processing (MDP) is a library of widely
+used data processing algorithms that can be combined according to a
+pipeline analogy to build more complex data processing software.
+
+From the user's perspective, MDP consists of a collection of
+supervised and unsupervised learning algorithms, and other data
+processing units (nodes) that can be combined into data processing
+sequences (flows) and more complex feed-forward network
+architectures. Given a set of input data, MDP takes care of
+successively training or executing all nodes in the network. This
+allows the user to specify complex algorithms as a series of simpler
+data processing steps in a natural way.
+
+The base of available algorithms is steadily increasing and includes,
+to name but the most common, Principal Component Analysis (PCA and
+NIPALS), several Independent Component Analysis algorithms (CuBICA,
+FastICA, TDSEP, JADE, and XSFA), Slow Feature Analysis, Gaussian
+Classifiers, Restricted Boltzmann Machine, and Locally Linear
+Embedding.
+
+Particular care has been taken to make computations efficient in terms
+of speed and memory.  To reduce memory requirements, it is possible to
+perform learning using batches of data, and to define the internal
+parameters of the nodes to be single precision, which makes the usage
+of very large data sets possible.  Moreover, the 'parallel' subpackage
+offers a parallel implementation of the basic nodes and flows.
+
+From the developer's perspective, MDP is a framework that makes the
+implementation of new supervised and unsupervised learning algorithms
+easy and straightforward.  The basic class, 'Node', takes care of
+tedious tasks like numerical type and dimensionality checking, leaving
+the developer free to concentrate on the implementation of the
+learning and execution phases. Because of the common interface, the
+node then automatically integrates with the rest of the library and
+can be used in a network together with other nodes. A node can have
+multiple training phases and even an undetermined number of phases.
+This allows the implementation of algorithms that need to collect some
+statistics on the whole input before proceeding with the actual
+training, and others that need to iterate over a training phase until
+a convergence criterion is satisfied. The ability to train each phase
+using chunks of input data is maintained if the chunks are generated
+with iterators. Moreover, crash recovery is optionally available: in
+case of failure, the current state of the flow is saved for later
+inspection.
+
+MDP has been written in the context of theoretical research in
+neuroscience, but it has been designed to be helpful in any context
+where trainable data processing algorithms are used. Its simplicity on
+the user side together with the reusability of the implemented nodes
+make it also a valid educational tool.
+
+http://mdp-toolkit.sourceforge.net
+"""
+
+classifiers = ["Development Status :: 5 - Production/Stable",
+               "Intended Audience :: Developers",
+               "Intended Audience :: Education",
+               "Intended Audience :: Science/Research",
+               "License :: OSI Approved :: GNU Library or "+\
+               "Lesser General Public License (LGPL)",
+               "Operating System :: OS Independent",
+               "Programming Language :: Python",
+               "Topic :: Scientific/Engineering :: Information Analysis",
+               "Topic :: Scientific/Engineering :: Mathematics"]
+
+
+setup(name = 'MDP', version = '2.5',
+      author = 'Pietro Berkes, Niko Wilbert, and Tiziano Zito',
+      author_email = 'berkes@brandeis.edu, mail@nikowilbert.de, tiziano.zito@bccn-berlin.de',
+      maintainer = 'Pietro Berkes, Niko Wilbert, and Tiziano Zito',
+      maintainer_email = 'berkes@brandeis.edu, mail@nikowilbert.de, tiziano.zito@bccn-berlin.de',
+      license = "http://www.gnu.org/licenses/lgpl.html",
+      platforms = ["Any"],
+      url = 'http://mdp-toolkit.sourceforge.net',
+      download_url = 'http://sourceforge.net/project/showfiles.php?group_id=116959',
+      description = short_description,
+      long_description = long_description,
+      classifiers = classifiers,
+      packages = ['mdp', 'mdp.nodes', 'mdp.utils', 'mdp.hinet',
+                  'mdp.test', 'mdp.demo', 'mdp.graph', 'mdp.contrib',
+                  'mdp.parallel'],
+      )
