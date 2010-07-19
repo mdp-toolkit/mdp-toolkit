FIX: Fixed the switchboard gradient and added unittest.

bimdp/nodes/gradient.py
@@ -80,6 +80,8 @@ class GradientExtensionNode(mdp.ExtensionNode, mdp.Node):
 # TODO: cache the gradient for linear nodes?
 #    If there was a linear base class one could integrate this?
 
+# TODO: at least at PCA implementation
+
 @mdp.extension_method("gradient", mdp.nodes.SFANode, "_get_grad")    
 def _sfa_grad(self, x):
     # the gradient is constant, but have to give it for each x point
@@ -140,7 +142,7 @@ def _layer_grad(self, x):
 # TODO: cache this gradient, like for linear nodes
 @mdp.extension_method("gradient", mdp.hinet.Switchboard, "_get_grad")    
 def _switchboard_grad(self, x):
-    grad = np.zeros(self.input_dim)
-    grad[self.connections] = 1
-    return np.tile(grad, (len(x), self.output_dim, 1))
+    grad = np.zeros((self.output_dim, self.input_dim))
+    grad[range(self.output_dim), self.connections] = 1
+    return np.tile(grad, (len(x), 1, 1))
 

bimdp/test/test_gradient.py
@@ -159,7 +159,21 @@ class TestGradientExtension(unittest.TestCase):
         finally:
             mdp.deactivate_extension("gradient")
             
-    def test_switchboard_gradient(self):
+    def test_switchboard_gradient1(self):
+        """Test that gradient is correct for a tiny switchboard."""
+        sboard = mdp.hinet.Switchboard(input_dim=4, connections=[2,0])
+        x = np.random.random((2,4))
+        mdp.activate_extension("gradient")
+        try:
+            result = sboard._gradient(x)
+            grad = result[1]["grad"]
+            ref_grad = np.array([[[0,0,1,0], [1,0,0,0]],
+                                 [[0,0,1,0], [1,0,0,0]]], dtype=grad.dtype)
+            self.assert_(np.all(grad == ref_grad))
+        finally:
+            mdp.deactivate_extension("gradient")
+
+    def test_switchboard_gradient2(self):
         """Test gradient for a simple switchboard."""
         sboard = mdp.hinet.Switchboard(input_dim=5, connections=[2,0,4])
         x = np.random.random((7,5))