4a4175fd5e89a9cfb711a8236f404fffc93580f5
Tiziano Zito <opossumnano@gmail.com>
2010-07-19 17:23:44 +0200

    FIX: missing import in setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from distutils.core import setup
+import os
 
 short_description = "Modular toolkit for Data Processing (MDP) is a library of widely used data processing algorithms that can be combined according to a pipeline analogy to build more complex data processing software. Implemented algorithms include Principal Component Analysis (PCA), Independent Component Analysis (ICA), Slow Feature Analysis (SFA), and many more."
 
