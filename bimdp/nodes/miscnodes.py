d1f43870c9fd74c43ce4c953774b22e3c45e947f
Niko Wilbert <mail@nikowilbert.de>
2010-07-20 13:01:03 +0200

    ERF: Removed the explicitly defined IdentityBiNode, use the autogenerated one instead.

--- bimdp/nodes/miscnodes.py
+++ bimdp/nodes/miscnodes.py
@@ -1,28 +1,6 @@
+
 from bimdp import BiNode, MSG_ID_SEP
-
-
-class IdentityBiNode(BiNode):
-    """Identity implementation for a BiNode.
-    
-    The arguments are simply passed through in execute.
-    
-    Instances of this class can be used as simple jump targets in a flow.
-    """
-    
-    def _execute(self, x):
-        """Return x and msg."""
-        return x
-    
-    def _set_input_dim(self, n):
-        """If input dim is given set output dim to the same value."""
-        self._input_dim = n
-        self.set_output_dim(n)
-    
-    def is_trainable(self):
-        return False
-    
-    def is_bi_training(self):
-        return False
+from autogen_binodes import IdentityBiNode
 
 
 class SenderBiNode(IdentityBiNode):
