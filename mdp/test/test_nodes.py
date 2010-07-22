7366a68802815e73eb1c2a7199d2817a2d083225
Zbyszek Szmek <zbyszek@in.waw.pl>
2010-07-22 10:45:14 +0200

    ERF: migrate tests for HistogramNode to py.test

--- mdp/test/test_nodes.py
+++ mdp/test/test_nodes.py
@@ -1373,25 +1373,6 @@ class NodesTestSuite(unittest.TestSuite):
         y = node.execute(x)
         assert numx.all(y==y_ref)
 
-    def testHistogramNode_nofraction(self):
-        """Test HistogramNode with fraction set to 1.0."""
-        node = mdp.nodes.HistogramNode()
-        x1 = numx.array([[0.1, 0.2], [0.3, 0.5]])
-        x2 = numx.array([[0.3, 0.6], [0.2, 0.1]])
-        x = numx.concatenate([x1, x2])
-        node.train(x1)
-        node.train(x2)
-        assert numx.all(x == node.data_hist)
-
-    def testHistogramNode_fraction(self):
-        """Test HistogramNode with fraction set to 0.5."""
-        node = mdp.nodes.HistogramNode(hist_fraction=0.5)
-        x1 = numx_rand.random((1000, 3))
-        x2 = numx_rand.random((500, 3))
-        node.train(x1)
-        node.train(x2)
-        assert len(node.data_hist) < 1000
-
 
 def get_suite(testname=None):
     return NodesTestSuite(testname=testname)
