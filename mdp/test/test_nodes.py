a6c6f604faeec6d5680c78e7fb4f60331faba12c
Zbyszek Szmek <zbyszek@in.waw.pl>
2010-07-22 10:33:18 +0200

    ERF: migrate tests for SFANode to py.test

--- mdp/test/test_nodes.py
+++ mdp/test/test_nodes.py
@@ -404,39 +404,6 @@ class NodesTestSuite(unittest.TestSuite):
         assert node.visited == [1, 2, 3, 4]
 
 
-
-    def testSFANode(self):
-        dim=10000
-        freqs = [2*numx.pi*1, 2*numx.pi*5]
-        t =  numx.linspace(0,1,num=dim)
-        mat = numx.array([numx.sin(freqs[0]*t),numx.sin(freqs[1]*t)]).T
-        mat = (mat - mean(mat[:-1,:],axis=0))\
-              /std(mat[:-1,:],axis=0)
-        des_mat = mat.copy()
-        mat = mult(mat,uniform((2,2))) + uniform(2)
-        sfa = mdp.nodes.SFANode()
-        sfa.train(mat)
-        out = sfa.execute(mat)
-        correlation = mult(des_mat[:-1,:].T,out[:-1,:])/(dim-2)
-        assert sfa.get_eta_values(t=0.5) is not None, 'get_eta is None'
-        assert_array_almost_equal(abs(correlation),
-                                  numx.eye(2), self.decimal-3)
-        sfa = mdp.nodes.SFANode(output_dim = 1)
-        sfa.train(mat)
-        out = sfa.execute(mat)
-        assert out.shape[1]==1, 'Wrong output_dim'
-        correlation = mult(des_mat[:-1,:1].T,out[:-1,:])/(dim-2)
-        assert_array_almost_equal(abs(correlation),
-                                  numx.eye(1), self.decimal-3)
-
-    def testSFANode_range_argument(self):
-        node = mdp.nodes.SFANode()
-        x = numx.random.random((100,10))
-        node.train(x)
-        node.stop_training()
-        y = node.execute(x, n=5)
-        assert y.shape[1] == 5
-
     def testSFA2Node(self):
         dim = 10000
         freqs = [2*numx.pi*100.,2*numx.pi*500.]
