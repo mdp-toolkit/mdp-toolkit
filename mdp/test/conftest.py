41ba14d65bcc8e385eb90b20619d84983eec0bfc
Zbyszek Szmek <zbyszek@in.waw.pl>
2010-07-23 17:34:49 +0200

    FIX: duplicate conftest.py twice, so py.test can be run without options, try 2
    Previous version worked only when PYTHONPATH included mdp-toolkit/.

--- mdp/test/conftest.py
+++ mdp/test/conftest.py
@@ -9,16 +9,38 @@ To rule this out, please run the tests more than once.
 If you get reproducible failures please report a bug!
 """
 
+def _have_option(parser, optionname):
+    return any(optionname == option.get_opt_string()
+               for option in parser._anonymous.options)
+
+def pytest_configure(config):
+    seed = config.getvalue("seed")
+    if seed is None or seed == ('NO', 'DEFAULT'):
+        config.option.seed = int(mdp.numx_rand.randint(2**31-1))
+
+def pytest_runtest_setup(item):
+    # set random seed
+    mdp.numx_rand.seed(item.config.option.seed)
+
+def pytest_addoption(parser):
+    """Add random seed option to py.test if it isn't already there
+    """
+    if not _have_option(parser, '--seed'):
+        parser.addoption('--seed', dest='seed', type=int, action='store',
+                         help='set random seed')
+
 try:
-    py.test.mdp_toolkit_configured
+    py.test.mdp_toolkit_reporting_configured
 except AttributeError:
+    # Global variable py.test.mdp_toolkit_reporting_configured is necessary
+    # to not create the pytest_* functions. Otherwise the terminal
+    # report would be printed twice: once for mdp/ and the second time
+    # for bimdp/test. When running tests just for one of the
+    # directories, the report is still printed correctly.
     def pytest_report_header(config):
-        # TODO: insert here information from the new mdp.info object
-        return '%s\nRandom Seed: %d\n' % (mdp.config.info(), config.option.seed,
-                                            )
+        return '%s\nRandom Seed: %d\n' % (mdp.config.info(), config.option.seed)
 
     def pytest_terminal_summary(terminalreporter):
-        # TODO: insert here information from the new mdp.info object
         t = terminalreporter
         t.write_sep("=", "NOTE")
         t.write_line("%s\nRandom Seed: %d" % (mdp.config.info(),
@@ -26,16 +48,5 @@ except AttributeError:
         if 'failed' in t.stats or 'error' in t.stats:
             t.write_line(_err_str)
 
-    def pytest_configure(config):
-        seed = config.getvalue("seed")
-        if seed is None or seed == ('NO', 'DEFAULT'):
-            config.option.seed = int(mdp.numx_rand.randint(2**31-1))
-
-    def pytest_runtest_setup(item):
-        # set random seed
-        mdp.numx_rand.seed(item.config.option.seed)
-
-    def pytest_addoption(parser):
-        """Add random seed option to py.test"""
-        parser.addoption("--seed", dest="seed", type="int", action="store",
-                         help="set random seed")
+    def pytest_namespace():
+        return dict(mdp_toolkit_reporting_configured=True)
