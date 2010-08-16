af47fee50580b3cc9642dfa2d574d06b5d7131f6
Pietro Berkes <pietro.berkes@googlemail.com>
2010-08-16 15:53:21 -0400

    FIX: py.test failed with an error reporting a duplicated 'seed' option
    In py.test 1.3.3 they changed the sequence in which the conftest.py
        files are loaded. In our case, the /mdp/test/conftest.py file was
        laoded before the /conftest.py file, which tried to redefine the
        optione 'seed', causing an error.

--- conftest.py
+++ conftest.py
@@ -1,8 +1,13 @@
 collect_ignore = ['build', 'cover', 'html',
                   ]
 
+def _have_option(parser, optionname):
+    return any(optionname == option.get_opt_string()
+               for option in parser._anonymous.options)
+
 def pytest_addoption(parser):
     """Add random seed option to py.test
     """
-    parser.addoption('--seed', dest='seed', type=int, action='store',
-                     help='set random seed')
+    if not _have_option(parser, '--seed'):
+        parser.addoption('--seed', dest='seed', type=int, action='store',
+                         help='set random seed')
