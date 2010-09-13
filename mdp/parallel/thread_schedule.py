4693387221befe73c8aafc6aeda79d6efdfc9f4f
Niko Wilbert <mail@nikowilbert.de>
2010-09-13 17:52:43 +0200

    ERF: Improved the threaded scheduler (better exception handling).

--- mdp/parallel/thread_schedule.py
+++ mdp/parallel/thread_schedule.py
@@ -52,20 +52,20 @@ class ThreadScheduler(Scheduler):
                 time.sleep(SLEEP_TIME)
                 self._lock.acquire()
             else:
+                self._lock.release()
+                task_callable = task_callable.fork()
+                # create a deep copy of the task_callable,
+                # since it might not be thread safe 
+                # (but the fork is still required)
+                as_str = pickle.dumps(task_callable, -1)
+                task_callable = pickle.loads(as_str)
                 try:
-                    self._lock.release()
-                    # create a deep copy of the task_callable,
-                    # since it might not be thread safe 
-                    as_str = pickle.dumps(task_callable, -1)
-                    task_callable = pickle.loads(as_str)
-                    # fork is still required
-                    task_callable = task_callable.fork()
                     thread = threading.Thread(target=self._task_thread,
                                               args=(data, task_callable,
                                                     task_index))
                     thread.start()
                     task_started = True
-                except thread.error:
+                except Exception:
                     if self.verbose:
                         print ("unable to create new thread,"
                                " waiting 2 seconds...")
