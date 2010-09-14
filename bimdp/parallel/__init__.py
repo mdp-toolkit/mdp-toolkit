b2b8069b8958ee09accfafbd741955455ee8e413
Niko Wilbert <mail@nikowilbert.de>
2010-09-14 17:58:46 +0200

    ERF: Removed the ParallelBiNode, since it is no longer needed.

--- bimdp/parallel/__init__.py
+++ bimdp/parallel/__init__.py
@@ -1,10 +1,8 @@
 
-from parallelbinode import ParallelExtensionBiNode
 from parallelbiflow import (
     BiFlowTrainCallable, BiFlowExecuteCallable,
     ParallelBiFlowException, ParallelBiFlow, ParallelCheckpointBiFlow)
 from parallelbihinet import ParallelCloneBiLayer
 
-del parallelbinode
 del parallelbiflow
 del parallelbihinet
