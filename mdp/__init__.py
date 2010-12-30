d96bb87ede162d15dd65d0a96f34eb93aae0caa1
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2010-12-30 13:58:16 +0100

    NEW: use context manager to simplify config

--- mdp/__init__.py
+++ mdp/__init__.py
@@ -53,6 +53,7 @@ make it also a valid educational tool.
 
 http://mdp-toolkit.sourceforge.net
 """
+from __future__ import with_statement
 __docformat__ = "restructuredtext en"
 
 class MDPException(Exception):
@@ -75,11 +76,13 @@ class config(object):
     series of attributes called 'has_<dependency>'.
 
     Dependency parameters are object which have a a boolean value
-    (``True`` if the dependency is available). If False, they contain an
-    error string which will be used in ``mdp.config.info()`` output. If
-    ``True``, they contain information about the available version of
-    the dependency. Those objects should be created by using the helper
-    class methods `ExternalDepFound` and `ExternalDepFailed`.
+    (``True`` if the dependency is available). If False, they contain
+    an error string which will be used in `mdp.config.info()`
+    output. If ``True``, they contain information about the available
+    version of the dependency. After the dependency object is created
+    its value should be set by calling `ExternalDep.found` or
+    `ExternalDep.failed`. Until one of those two methods is called,
+    the object is presented as SKIPPED.
 
     Dependency parameters are numbered in the order of creation,
     so the output is predictable.
@@ -89,12 +92,18 @@ class config(object):
     """
 
     _HAS_NUMBER = 0
-    class _ExternalDep(object):
-        def __init__(self, name, version=None, failmsg=None):
-            assert (version is not None) + (failmsg is not None) == 1
+    class ExternalDep(object):
+        def __init__(self, name):
+            """
+            :Parameters:
+              name
+                identifier of the optional dependency. This should
+                be a valid python identifier, because it will be
+                accessible as ``mdp.config.has_<name>`` attribute.
+            """
 
-            self.version = str(version) # convert e.g. exception to str
-            self.failmsg = str(failmsg) if failmsg is not None else None
+            self.version = None
+            self.failmsg = None
 
             global config
             self.order = config._HAS_NUMBER
@@ -102,65 +111,72 @@ class config(object):
             setattr(config, 'has_' + name, self)
 
         def __nonzero__(self):
-            return self.failmsg is None
+            return self.version is not None
 
         def __repr__(self):
-            if self:
+            if self.version is not None:
                 return self.version
+            elif self.failmsg is not None:
+                return "NOT AVAILABLE: " + str(self.failmsg)
             else:
-                return "NOT AVAILABLE: " + self.failmsg
+                return "SKIPPED"
 
-    @classmethod
-    def ExternalDepFailed(cls, name, failmsg):
-        """Inform that an optional dependency was not found.
+        def found(self, version):
+            """Inform that an optional dependency was found.
 
-        A new `_ExternalDep` object will be created and stored
-        in `config`.
+            :Parameters:
+              version
+                an object convertible to ``str``, which will be displayed in
+                ``mdp.config.info()`` output. Something like ``'0.4.3'``.
+            """
+            if version is None:
+                raise ValueError('version cannot be None')
+            if self.failmsg is not None:
+                raise ValueError("failmsg was previously set to '%s'" % self.failmsg)
+            self.version = str(version)
 
-        :Parameters:
-          name
-            identifier of the optional dependency. This should
-            be a valid python identifier, because it will be
-            accessible as ``mdp.config.has_<name>`` attribute.
-          failmsg
-            an object convertible to ``str``, which will be displayed in
-            ``mdp.config.info()`` output. This will usually be either an
-            exception (e.g. ``ImportError``), or a message string.
-        """
-        return cls._ExternalDep(name, failmsg=failmsg)
+        def failed(self, failmsg):
+            """Inform that an optional dependency was not found.
 
-    @classmethod
-    def ExternalDepFound(cls, name, version):
-        """Inform that an optional dependency was found.
+            The value of this `ExternalDep` object stored in `config`
+            will be ``False``.
 
-        A new `_ExternalDep` object will be created and stored
-        in `config`.
+            :Parameters:
+              failmsg
+                an object convertible to ``str``, which will be displayed in
+                ``mdp.config.info()`` output. This will usually be either an
+                exception (e.g. ``ImportError``), or a message string.
+            """
+            if failmsg is None:
+                raise ValueError('failmsg cannot be None')
+            if self.version is not None:
+                raise ValueError("version was previously set to '%s'" % self.version)
+            self.failmsg = failmsg
 
-        :Parameters:
-          name
-            identifier of the optional dependency. This should
-            be a valid python identifier, because it will be
-            accessible as ``mdp.config.has_<name>`` attribute.
-          version
-            an object convertible to ``str``, which will be displayed in
-            ``mdp.config.info()`` output. Something like ``'0.4.3'``.
-        """
-        return cls._ExternalDep(name, version=version)
+        def __enter__(self):
+            return self
+
+        def __exit__(self, type, value, traceback):
+            if type is not None:
+                self.failed(value)
+                return True
 
     @classmethod
     def info(cls):
         """Return nicely formatted info about MDP.
 
         >>> print mdp.config.info()                           # doctest: +SKIP
-                  python: 2.6.6.final.0
-                   scipy: 0.7.2
-            scipy signal: 0.7.2
-         parallel python: 1.6.0
-                  shogun: v0.9.3_r4889_2010-05-27_20:52_
-                  libsvm: NOT AVAILABLE: No module named svm
-              new symeig: NOT AVAILABLE: symeig version too old
-                  symeig: wrap_eigh
-                  joblib: 0.4.6
+                    python: 2.6.6.final.0
+                     scipy: 0.7.2
+                     numpy: NOT AVAILABLE: MDPNUMX set to scipy
+              scipy signal: 0.7.2
+           parallel python: 1.6.0
+                    shogun: v0.9.3_r4889_2010-05-27_20:52_
+                    libsvm: NOT AVAILABLE: No module named svm
+                    symeig: wrap_eigh
+                new symeig: numx_linalg.eigh
+         standalone symeig: SKIPPED
+                    joblib: 0.4.6
 
         This function is used to provide the py.test report header and
         footer.
@@ -174,7 +190,7 @@ class config(object):
 
 import sys, os
 
-config.ExternalDepFound('python', '.'.join([str(x) for x in sys.version_info]))
+config.ExternalDep('python').found('.'.join([str(x) for x in sys.version_info]))
 
 # To force MDP to use one specific extension module
 # set the environment variable MDPNUMX
@@ -187,66 +203,52 @@ Supported backends: numpy, scipy.""" % _USR_LABEL
     raise ImportError(err)
 
 numx_description = None
-numx_exceptions = {}
 
-if _USR_LABEL and _USR_LABEL != 'scipy':
-    config.ExternalDepFailed('scipy', 'MDPNUMX set to %s' % _USR_LABEL)
-else:
-    try:
+with config.ExternalDep('scipy') as dep:
+    if _USR_LABEL and _USR_LABEL != 'scipy':
+        dep.failed('MDPNUMX set to %s' % _USR_LABEL)
+    else:
         import scipy as numx
         from scipy import (linalg as numx_linalg,
                            fftpack as numx_fft,
                            random as numx_rand,
                            version as numx_version)
-    except ImportError, exc:
-        numx_exceptions['scipy'] = exc
-        config.ExternalDepFailed('scipy', exc)
-    else:
         numx_description = 'scipy'
-        config.ExternalDepFound('scipy', numx_version.version)
+        dep.found(numx_version.version)
 
-if _USR_LABEL and _USR_LABEL != 'numpy':
-    config.ExternalDepFailed('numpy', 'MDPNUMX set to %s' % _USR_LABEL)
-elif numx_description is None:
-    try:
+with config.ExternalDep('numpy') as dep:
+    if _USR_LABEL and _USR_LABEL != 'numpy':
+        dep.failed('MDPNUMX set to %s' % _USR_LABEL)
+    elif numx_description is None:
         import numpy as numx
         from numpy import (linalg as numx_linalg,
                            fft as numx_fft,
                            random as numx_rand,
                            version as numx_version)
         numx_description = 'numpy'
-        config.ExternalDepFound('numpy', numx_version.version)
-    except ImportError, exc:
-        config.ExternalDepFailed('numpy', exc)
-        numx_exceptions['numpy'] = exc
-else:
-    config.ExternalDepFailed('numpy', 'scipy is preferred')
+        dep.found(numx_version.version)
+    else:
+        dep.failed('scipy is preferred')
 
-assert config.has_scipy != config.has_numpy
+assert bool(config.has_scipy) != bool(config.has_numpy)
 
 if numx_description is None:
     # The test is for numx_description, not numx, because numx could
     # be imported sucessfuly, but e.g. numx_rand could later fail.
     msg = ("Could not import any of the numeric backends.\n"
            "Import errors:\n"
-           + '\n'.join(label+': '+str(exc)
-                       for label, exc in numx_exceptions.iteritems()))
+           "numpy:" + str(config.has_numpy.failmsg) + "\n"
+           "scipy:" + str(config.has_scipy.failmsg) + "\n")
     raise ImportError(msg)
-else:
-    # we have numx, we don't need the exceptions anymore
-    del numx_exceptions
 
 del _USR_LABEL
 
-if config.has_scipy:
-    try:
+with config.ExternalDep('scipy_signal') as dep:
+    if config.has_scipy:
         import scipy.signal
-    except ImportError, exc:
-        config.ExternalDepFailed('scipy_signal', exc)
+        dep.found(scipy.version.version)
     else:
-        config.ExternalDepFound('scipy_signal', scipy.version.version)
-else:
-    config.ExternalDepFailed('scipy_signal', 'scipy not available')
+        dep.failed('scipy not available')
 
 # import the utils module (used by other modules)
 # here we set scipy_emulation if needed.
@@ -259,60 +261,56 @@ __copyright__ = '(c) 2003-2010 Pietro Berkes, Rike-Benjamin Schuppner, Niko Wilb
 __license__ = 'Modified BSD License (GPL compatible), see COPYRIGHT'
 __contact__ = 'mdp-toolkit-users@lists.sourceforge.net'
 
-try:
+with config.ExternalDep('parallel_python') as dep:
     import pp
-except ImportError, exc:
-    config.ExternalDepFailed('parallel_python', exc)
-else:
-    config.ExternalDepFound('parallel_python', pp.version)
+    dep.found(pp.version)
 
-try:
+with config.ExternalDep('shogun') as dep:
     from shogun import (Kernel as sgKernel,
                         Features as sgFeatures,
                         Classifier as sgClassifier)
-except ImportError, exc:
-    config.ExternalDepFailed('shogun', exc)
-else:
     # We need to have at least SHOGUN 0.9, as we rely on
     # SHOGUN's CClassifier::classify() method.
     # (It makes our code much nicer, by the way.)
     #
     if not hasattr(sgClassifier.Classifier, 'classify'):
-        config.ExternalDepFailed('shogun', "CClassifier::classify not found")
-    try:
-        version = sgKernel._Kernel.Version_get_version_release()
-    except AttributeError, msg:
-        config.ExternalDepFailed('shogun', msg)
-    else:
-        if not (version.startswith('v0.9') or version.startswith('v1.')):
-            config.ExternalDepFailed('We need at least SHOGUN version 0.9.')
-        config.ExternalDepFound('shogun', version)
+        raise ValueError('CClassifier::classify not found')
+    version = sgKernel._Kernel.Version_get_version_release()
+    if not (version.startswith('v0.9') or version.startswith('v1.')):
+        raise ValueError('We need at least SHOGUN version 0.9.')
+    dep.found(version)
 
-try:
+with config.ExternalDep('libsvm') as dep:
     import svm as libsvm
-except ImportError, exc:
-    config.ExternalDepFailed('libsvm', exc)
-else:
-    config.ExternalDepFound('libsvm', libsvm.libsvm._name)
+    dep.found(libsvm.libsvm._name)
 
 import inspect as _inspect
-try:
-    # check if scipy.linalg.eigh is the new version
-    # if yes, just wrap it
-    args = _inspect.getargspec(numx_linalg.eigh)[0]
-    if len(args) <= 4:
-        from symeig import symeig, SymeigException
-    else:
-        config.ExternalDepFailed('new_symeig', 'symeig version too old')
-        from utils._symeig import (wrap_eigh as symeig,
+with config.ExternalDep('symeig') as dep:
+    with config.ExternalDep('new_symeig') as dep2:
+        # check if scipy.linalg.eigh is the new version
+        # if yes, just wrap it
+        args = _inspect.getargspec(numx_linalg.eigh)[0]
+        if len(args) > 4:
+            dep.found('wrap_eigh')
+            dep2.found('numx_linalg.eigh')
+            from utils._symeig import (wrap_eigh as symeig,
+                                       SymeigException)
+        else:
+            dep2.failed('numx_linalg.eigh version too old')
+
+    with config.ExternalDep('standalone_symeig') as dep2:
+        if not config.has_new_symeig:
+            from symeig import symeig, SymeigException
+            dep.found(symeig.__name__)
+            dep2.found(symeig.__name__)
+
+    del dep2
+
+    if not config.has_new_symeig and not config.has_standalone_symeig:
+        dep.failed('using fake symeig')
+        from utils._symeig import (_symeig_fake as symeig,
                                    SymeigException)
 
-    config.ExternalDepFound('symeig', symeig.__name__)
-except ImportError, exc:
-    config.ExternalDepFailed('symeig', exc)
-    from utils._symeig import (_symeig_fake as symeig,
-                               SymeigException)
-    config.ExternalDepFound('symeig_fake', symeig.__name__)
 
 # import exceptions from nodes and flows
 from signal_node import (NodeException, InconsistentDimException,
@@ -374,12 +372,11 @@ __all__ = ['CheckpointFlow', 'CheckpointFunction', 'CheckpointSaveFunction',
            'config'
            ]
 
-try:
+with config.ExternalDep('joblib') as dep:
     import joblib
     __all__ += ['joblib']
-    config.ExternalDepFound('joblib', joblib.__version__)
-except ImportError, exc:
-    config.ExternalDepFailed('joblib', exc)
+    dep.found(joblib.__version__)
+del dep
 
 if config.has_joblib:
     import caching
--- mdp/test/test_config.py
+++ mdp/test/test_config.py
@@ -3,39 +3,43 @@
 from mdp import config
 
 def test_config_numpy_or_scipy():
-    assert config.has_scipy or config.has_numpy
+    assert bool(config.has_scipy) != bool(config.has_numpy)
 
 class TestConfig(object):
     def teardown_method(self, method):
         delattr(config, 'has_test_property')
 
     def test_config_depfound(self):
-        s = config.ExternalDepFound('test_property', 0.777)
-        assert bool(s) == True
+        dep = config.ExternalDep('test_property')
+        dep.found(0.777)
+        assert bool(dep) == True
         assert config.has_test_property
         info = config.info()
         assert 'test property' in info
         assert '0.777' in info
 
     def test_config_depfound_string(self):
-        s = config.ExternalDepFound('test_property', '0.777')
-        assert bool(s) == True
+        dep = config.ExternalDep('test_property')
+        dep.found('0.777')
+        assert bool(dep) == True
         assert config.has_test_property
         info = config.info()
         assert 'test property' in info
         assert '0.777' in info
 
     def test_config_depfailed_exc(self):
-        s = config.ExternalDepFailed('test_property', ImportError('GOOGOO'))
-        assert bool(s) == False
+        dep = config.ExternalDep('test_property')
+        dep.failed(ImportError('GOOGOO'))
+        assert bool(dep) == False
         assert not config.has_test_property
         info = config.info()
         assert 'test property' in info
         assert 'GOOGOO' in info
 
     def test_config_depfailed_string(self):
-        s = config.ExternalDepFailed('test_property', 'GOOGOO')
-        assert bool(s) == False
+        dep = config.ExternalDep('test_property')
+        dep.failed('GOOGOO')
+        assert bool(dep) == False
         assert not config.has_test_property
         info = config.info()
         assert 'test property' in info
