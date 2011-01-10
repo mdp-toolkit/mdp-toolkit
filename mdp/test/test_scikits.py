4ea6dbcce5116b6aee3c51859b9dda96fc6a4e01
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2011-01-10 13:06:31 +0100

    FIX: move scikits test into mdp/tests/

---
+++ mdp/test/test_scikits.py
@@ -0,0 +1,25 @@
+from _tools import *
+
+requires_scikits = skip_on_condition(
+    "not mdp.config.has_scikits",
+    "This test requires Scikits")
+
+@requires_scikits
+def test_scikits_PCANode_training():
+    node = mdp.nodes.scikits.PCANode(output_dim=3)
+    x = numx.concatenate((numx_rand.randn(100, 10),
+                          10 + numx_rand.randn(300, 10)))
+    node.train(x)
+    node.stop_training()
+    y = node.execute(x)
+
+    x.shape, y.shape
+    #node.prob(x)
+
+#GMMNode = wrap_scikits_alg(scikits.learn.glm.ElasticNet)
+#node = GMMNode(alpha=0., dtype='f')
+#x = numpy.random.rand(100,5).astype('f')
+#y = 0.3 * x[:,1] + 0.7 * x[:,4]
+#node.train(x, y)
+#node.stop_training()
+#z = node.execute(x)
