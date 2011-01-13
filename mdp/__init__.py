140448d188914c533e59a44cc32362c619d67d61
Tiziano Zito <opossumnano@gmail.com>
2011-01-13 15:34:02 +0100

    NEW: mdp descriptions now defined in the module.
    They new descriptions are used in the setup.py script and
    in generating the web site.

--- mdp/__init__.py
+++ mdp/__init__.py
@@ -1,17 +1,94 @@
 # Modular toolkit for Data Processing (MDP)
 """
-The Modular toolkit for Data Processing (MDP) is a library of widely
-used data processing algorithms that can be combined according to a
-pipeline analogy to build more complex data processing software.
+**The Modular toolkit for Data Processing (MDP)** package is a library
+of widely used data processing algorithms, and the possibility to
+combine them together to form pipelines for building more complex
+data processing software.
 
-From the user's perspective, MDP consists of a collection of
-supervised and unsupervised learning algorithms, and other data
-processing units (nodes) that can be combined into data processing
-sequences (flows) and more complex feed-forward network
-architectures. Given a set of input data, MDP takes care of
-successively training or executing all nodes in the network. This
-allows the user to specify complex algorithms as a series of simpler
-data processing steps in a natural way.
+MDP has been designed to be used as-is and as a framework for
+scientific data processing development.
+
+From the user's perspective, MDP consists of a collection of *units*,
+which process data. For example, these include algorithms for
+supervised and unsupervised learning, principal and independent
+components analysis and classification.
+
+These units can be chained into data processing flows, to create
+pipelines as well as more complex feed-forward network
+architectures. Given a set of input data, MDP takes care of training
+and executing all nodes in the network in the correct order and
+passing intermediate data between the nodes. This allows the user to
+specify complex algorithms as a series of simpler data processing
+steps.
+
+The number of available algorithms is steadily increasing and includes
+signal processing methods (Principal Component Analysis, Independent
+Component Analysis, Slow Feature Analysis), manifold learning methods
+([Hessian] Locally Linear Embedding), several classifiers,
+probabilistic methods (Factor Analysis, RBM), data pre-processing
+methods, and many others.
+
+Particular care has been taken to make computations efficient in terms
+of speed and memory. To reduce the memory footprint, it is possible to
+perform learning using batches of data. For large data-sets, it is
+also possible to specify that MDP should use single precision floating
+point numbers rather than double precision ones. Finally, calculations
+can be parallelised using the ``parallel`` subpackage, which offers a
+parallel implementation of the basic nodes and flows.
+
+From the developer's perspective, MDP is a framework that makes the
+implementation of new supervised and unsupervised learning algorithms
+easy and straightforward. The basic class, ``Node``, takes care of tedious
+tasks like numerical type and dimensionality checking, leaving the
+developer free to concentrate on the implementation of the learning
+and execution phases. Because of the common interface, the node then
+automatically integrates with the rest of the library and can be used
+in a network together with other nodes.
+
+A node can have multiple training phases and even an undetermined
+number of phases. Multiple training phases mean that the training data
+is presented multiple times to the same node. This allows the
+implementation of algorithms that need to collect some statistics on
+the whole input before proceeding with the actual training, and others
+that need to iterate over a training phase until a convergence
+criterion is satisfied. It is possible to train each phase using
+chunks of input data if the chunks are given as an iterable. Moreover,
+crash recovery can be optionally enabled, which will save the state of
+the flow in case of a failure for later inspection.
+
+MDP is distributed under the open source BSD license. It has been
+written in the context of theoretical research in neuroscience, but it
+has been designed to be helpful in any context where trainable data
+processing algorithms are used. Its simplicity on the user's side, the
+variety of readily available algorithms, and the reusability of the
+implemented nodes also make it a useful educational tool.
+
+http://mdp-toolkit.sourceforge.net
+"""
+__docformat__ = "restructuredtext en"
+
+__short_description__ = (
+    "MDP is a Python library of widely used data processing algorithms "
+    "that can be combined according to a pipeline analogy to build more "
+    "complex data processing software. The base of available algorithms "
+    "includes signal processing methods (Principal Component Analysis, "
+    "Independent Component Analysis, Slow Feature Analysis), "
+    "manifold learning methods ([Hessian] Locally Linear Embedding), "
+    "several classifiers, probabilistic methods (Factor Analysis, RBM), "
+    "data pre-processing methods, and many others."
+    )
+
+__medium_description__ ="""
+**Modular toolkit for Data Processing (MDP)** is a Python data processing framework.
+
+From the user's perspective, MDP is a collection of supervised and unsupervised
+learning algorithms and other data processing units that can be combined into
+data processing sequences and more complex feed-forward network architectures.
+
+From the scientific developer's perspective, MDP is a modular framework, which
+can easily be expanded. The implementation of new algorithms is easy and
+intuitive. The new implemented units are then automatically integrated with
+the rest of the library.
 
 The base of available algorithms is steadily increasing and includes
 signal processing methods (Principal Component Analysis,
@@ -19,52 +96,7 @@ Independent Component Analysis, Slow Feature Analysis),
 manifold learning methods ([Hessian] Locally Linear Embedding),
 several classifiers, probabilistic methods (Factor Analysis, RBM),
 data pre-processing methods, and many others.
-
-Particular care has been taken to make computations efficient in terms
-of speed and memory.  To reduce memory requirements, it is possible to
-perform learning using batches of data, and to define the internal
-parameters of the nodes to be single precision, which makes the usage
-of very large data sets possible.  Moreover, the 'parallel' subpackage
-offers a parallel implementation of the basic nodes and flows.
-
-From the developer's perspective, MDP is a framework that makes the
-implementation of new supervised and unsupervised learning algorithms
-easy and straightforward.  The basic class, 'Node', takes care of
-tedious tasks like numerical type and dimensionality checking, leaving
-the developer free to concentrate on the implementation of the
-learning and execution phases. Because of the common interface, the
-node then automatically integrates with the rest of the library and
-can be used in a network together with other nodes. A node can have
-multiple training phases and even an undetermined number of phases.
-This allows the implementation of algorithms that need to collect some
-statistics on the whole input before proceeding with the actual
-training, and others that need to iterate over a training phase until
-a convergence criterion is satisfied. The ability to train each phase
-using chunks of input data is maintained if the chunks are generated
-with iterators. Moreover, crash recovery is optionally available: in
-case of failure, the current state of the flow is saved for later
-inspection.
-
-MDP has been written in the context of theoretical research in
-neuroscience, but it has been designed to be helpful in any context
-where trainable data processing algorithms are used. Its simplicity on
-the user side together with the reusability of the implemented nodes
-make it also a valid educational tool.
-
-http://mdp-toolkit.sourceforge.net
 """
-__docformat__ = "restructuredtext en"
-
-__short_description__ = (
-    "MDP is a Python library of widely used data processing algorithms"
-    "that can be combined according to a pipeline analogy to build more"
-    "complex data processing software. The base of available algorithms"
-    "includes signal processing methods (Principal Component Analysis,"
-    "Independent Component Analysis, Slow Feature Analysis),"
-    "manifold learning methods ([Hessian] Locally Linear Embedding),"
-    "several classifiers, probabilistic methods (Factor Analysis, RBM),"
-    "data pre-processing methods, and many others."
-    )
 
 class MDPException(Exception):
     """Base class for exceptions in MDP."""
