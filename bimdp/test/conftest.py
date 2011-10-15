0ca4034a04c912884254b3a3b4bf9683d64f7cba
Tiziano Zito <opossumnano@gmail.com>
2011-10-15 18:07:06 +0200

    Revert "Revert back to original triple conftest.py setup"
    Revert back to use only two conftest.py file. The reasoning
    being that the added value of the three file configuration is not worth
    the higher complexity of the code that is needed to support it.
    
    The main advantage of having the configuration that we revert here is
    that you can run py.test --seed N (without additional arguments) in
    the MDP top-level directory. With the configuration introduced by the
    present commit, you will need to run "py.test --seed N mdp" or
    "py.test --seed N mdp bimdp" [this is to force py.test to load the
    mdp/test/conftest.py where the seed option has been defined].
    Note that if you do not need the seed option, you can stil run pu.test
    without arguments.
    
    For the end user there's no difference, as they should keep on using:
    
    >>> import mdp
    >>> mdp.test(seed=N)
    >>> import bimdp
    >>> bimdp.test(seed=N)
    
    which has always worked.
    
    It seems that this issue only affects and bothers Zbyszek and me, so the
    rest of the world can ignore the whole thing ;)
    
    This reverts commit e2c180055bea33a0114cac462dbc460e999d2376.

--- bimdp/test/conftest.py
+++ bimdp/test/conftest.py
@@ -6,25 +6,12 @@ import glob
 import mdp
 import py.test
 
-###################################################################
-### After changing this file, please copy it to
-### ../../mdp/test/conftest.py. The content of those two files
-### should be identical, but they cannot be exactly the same.
-### Otherwise py.test >= 1.3 skips one of them.
-### E.g.
-###  sed -r 's+mdp/+TMP\/+g; s+bimdp/+mdp/+g; s+TMP\/+bimdp/+g' <bimdp/test/conftest.py >mdp/test/conftest.py
-###################################################################
-
 _err_str = """
 IMPORTANT: some tests use random numbers. This could
 occasionally lead to failures due to numerical degeneracies.
 To rule this out, please run the tests more than once.
 If you get reproducible failures please report a bug!
 """
-
-def _have_option(parser, optionname):
-    return any(optionname == option.get_opt_string()
-               for option in parser._anonymous.options)
 
 def pytest_configure(config):
     seed = config.getvalue("seed")
@@ -40,7 +27,7 @@ def pytest_unconfigure(config):
     # if pp was monkey-patched, remove any stale pp4mdp directories
     if mdp.config.pp_monkeypatch_dirname:
         monkey_dirs = os.path.join(mdp.config.pp_monkeypatch_dirname,
-                                   mdp.parallel.pp_support.TEMPDIR_PREFIX)
+                                   mdp.parallel.pp_support.TEMPDIR_PREFIX) 
         [shutil.rmtree(d, ignore_errors=True) for d in glob.glob(monkey_dirs+'*')]
 
 def pytest_runtest_setup(item):
@@ -51,36 +38,27 @@ def pytest_runtest_setup(item):
     mdp.numx_rand.seed(item.config.option.seed)
 
 def pytest_addoption(parser):
-    """Add random seed option to py.test if it isn't already there
+    """Add random seed option to py.test.
     """
-    if not _have_option(parser, '--seed'):
-        parser.addoption('--seed', dest='seed', type=int, action='store',
-                         help='set random seed')
+    parser.addoption('--seed', dest='seed', type=int, action='store',
+                     help='set random seed')
 
-try:
-    py.test.mdp_configured
-except AttributeError:
-    # Global variable py.test.mdp_configured is necessary
-    # to not create the pytest_* functions. Otherwise the terminal
-    # report would be printed twice: once for bimdp/ and the second time
-    # for mdp/test. When running tests just for one of the
-    # directories, the report is still printed correctly.
-    def pytest_report_header(config):
-        # report the random seed before and after running the tests
-        return '%s\nRandom Seed: %d\n' % (mdp.config.info(), config.option.seed)
+def pytest_report_header(config):
+    # report the random seed before and after running the tests
+    return '%s\nRandom Seed: %d\n' % (mdp.config.info(), config.option.seed)
 
-    def pytest_terminal_summary(terminalreporter):
-        # add a note about error due to randomness only if an error or
-        # a failure occured
-        t = terminalreporter
-        t.write_sep("=", "NOTE")
-        t.write_line("%s\nRandom Seed: %d" % (mdp.config.info(),
-                                              t.config.option.seed))
-        if 'failed' in t.stats or 'error' in t.stats:
-            t.write_line(_err_str)
+def pytest_terminal_summary(terminalreporter):
+    # add a note about error due to randomness only if an error or a failure
+    # occured
+    t = terminalreporter
+    t.write_sep("=", "NOTE")
+    t.write_line("%s\nRandom Seed: %d" % (mdp.config.info(),
+                                          t.config.option.seed))
+    if 'failed' in t.stats or 'error' in t.stats:
+        t.write_line(_err_str)
 
-    def pytest_namespace():
-        # get temporary directory to put temporary files
-        # will be deleted at the end of the test run
-        dirname = tempfile.mkdtemp(suffix='.tmp', prefix='MDPtestdir_')
-        return dict(mdp_configured=True, mdp_tempdirname=dirname)
+def pytest_namespace():
+    # get temporary directory to put temporary files
+    # will be deleted at the end of the test run
+    dirname = tempfile.mkdtemp(suffix='.tmp', prefix='MDPtestdir_')
+    return dict(mdp_tempdirname=dirname)
