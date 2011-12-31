0f8db226326999a3881692b80249be2735136d49
Zbyszek Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2011-12-31 01:26:31 +0100

    ENH: recurse into class attributes when doing namespace fixups
    After this change, methods like mdp.nodes.IdentityNode.__init__ have
    the same __module__ attribute as the class to which they are
    attached. This increases consistency, and has the additional advantage
    that when epydoc looks at the methods and their __module__ attributes,
    it is not confused by un-reachable modules.
    
    Removes epydoc messages like:
    Warning: __init__'s docstring is not a unicode string, but it contains
      non-ascii data -- treating it as latin-1.
    
    ver. 2: add documentation string

--- mdp/utils/__init__.py
+++ mdp/utils/__init__.py
@@ -1,3 +1,5 @@
+__docformat__ = "restructuredtext en"
+
 from routines import (timediff, refcast, scast, rotate, random_rot,
                       permute, symrand, norm2, cov2,
                       mult_diag, comb, sqrtm, get_dtypes, nongeneral_svd,
@@ -93,7 +95,7 @@ __all__ = ['CovarianceMatrix', 'DelayCovarianceMatrix','CrossCovarianceMatrix',
            'weighted_choice', 'bool_to_sign', 'sign_to_bool',
            'OrderedDict', 'TemporaryDirectory', 'gabor', 'fixup_namespace']
 
-def without_prefix(name, prefix):
+def _without_prefix(name, prefix):
     if name.startswith(prefix):
         return name[len(prefix):]
     else:
@@ -103,6 +105,46 @@ import os
 FIXUP_DEBUG = os.getenv('MDPNSDEBUG')
 
 def fixup_namespace(mname, names, old_modules, keep_modules=()):
+    """Update ``__module__`` attribute and remove ``old_modules`` from namespace
+
+    When classes are imported from implementation modules into the
+    package exporting them, the ``__module__`` attribute reflects the
+    place of definition. Splitting the code into separate files (and
+    thus modules) makes the implementation managable. Nevertheless, we
+    do not want the implementation modules to be visible and delete
+    their names from the package's namespace. This causes some
+    problems: when looking at the exported classes and other objects,
+    their ``__module__`` attribute points to something non-importable,
+    ``repr`` output and documentation do not show the module from
+    which they are supposed to be imported. The documentation
+    generators like epydoc and sphinx are also confused. To alleviate
+    those problems, the ``__module__`` attributes of all exported
+    classes defined in a "private" module and then exported elsewhere
+    are changed to the latter.
+
+    For each name in ``names``, if ``<mname>.<name>`` is accessible,
+    and if its ``__module__`` attribute is equal to one of the names
+    in ``old_modules``, it is changed to ``"<mname>"``. In other
+    words, all the ``__module__`` attributes of objects exported from
+    module ``<mname>`` are updated, iff they used to point to one of the
+    "private" modules in ``old_modules``.
+
+    This operation is performed not only for classes, but actually for
+    all objects with the ``__module__`` attribute, following the rules
+    stated above. The operation is also performed recursively, not
+    only for names in ``names``, but also for methods, inner classes,
+    and other attributes. This recursive invocation is necessary
+    because all the problems affecting top-level exported classes also
+    affect their attributes visible for the user, and especially
+    documented functions.
+
+    If ``names`` is ``None``, all public names in module ``<mname>``
+    (not starting with ``'_'``) are affected.
+
+    After the ``__module__`` attributes are changed, "private" modules given
+    in ``old_modules``, except for the ones in ``keep_modules``, are deleted
+    from the namespace of ``<mname>`` module.
+    """
     import sys
     module = sys.modules[mname]
     if names is None:
@@ -110,13 +152,8 @@ def fixup_namespace(mname, names, old_modules, keep_modules=()):
     if FIXUP_DEBUG:
         print 'NAMESPACE FIXUP: %s (%s)' % (module, mname)
     for name in names:
-        item = getattr(module, name)
-        if (hasattr(item, '__module__') and
-            without_prefix(item.__module__, mname + '.') in old_modules):
-            if FIXUP_DEBUG:
-                print 'namespace fixup: {%s => %s}.%s' % (
-                    item.__module__, mname, item.__name__)
-            item.__module__ = mname
+        _fixup_namespace_item(module, mname, name, old_modules, '')
+
     # take care of removing the module filenames
     for filename in old_modules:
         # skip names in keep modules
@@ -130,6 +167,36 @@ def fixup_namespace(mname, names, old_modules, keep_modules=()):
             # if the name is not there, we are in a reload, so do not
             # do anything
             pass
+
+def _fixup_namespace_item(parent, mname, name, old_modules, path):
+    try:
+        item = getattr(parent, name)
+    except AttributeError:
+        if name.startswith('__'): # those sometimes fail unexplicably
+            return
+        else:
+            raise
+    current_name = getattr(item, '__module__', None)
+    if (current_name is not None and
+        _without_prefix(current_name, mname + '.') in old_modules):
+        if FIXUP_DEBUG:
+            print 'namespace fixup: {%s => %s}%s.%s' % (
+                current_name, mname, path, name)
+        try:
+            item.__module__ = mname
+        except AttributeError:
+            try:
+                item.im_func.__module__ = mname
+            except AttributeError, e:
+                if FIXUP_DEBUG:
+                    print 'namespace fixup failed: ', e
+            # don't recurse into functions anyway
+            return
+        subitems = [_name for _name in dir(item)
+                    if _name.startswith('__') or not _name.startswith('_')]
+        for subitem in subitems:
+            _fixup_namespace_item(item, mname, subitem, old_modules,
+                                  path + '.' + name)
 
 fixup_namespace(__name__, __all__,
                 ('routines',
