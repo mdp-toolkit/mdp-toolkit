52c746d4f653e51dbbbb2f8451edd6e5f28222e4
Noah Young <noahpyoung@gmail.com>
2013-07-26 11:26:01 -0700

    Finished test to check if the mdp.utils.mult function gets switched to theano's and back. Added test to see if theano gets registered as an active extension.

--- mdp/test/test_theano.py
+++ mdp/test/test_theano.py
@@ -1,4 +1,14 @@
 """Test gputheano extension."""
+import theano
+
+def test_activate_deactivate():
+    mdp.gputheano.activate_theano()
+    assert 'theano' in mdp.get_active_extensions()
+    mdp.gputheano.deactivate_theano()
+    assert 'theano' not in mdp.get_active_extensions()
+
 def test_mult_remapping():
-    with mdp.theano.theanoize():
-        
+    mdp.gputheano.activate_theano()
+    assert type(mdp.utils.mult) == theano.compile.function_module.Function
+    mdp.gputheano.deactivate_theano()
+    assert mdp.utils.mult == np.dot
