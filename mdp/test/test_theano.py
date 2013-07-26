4876e62281cef623eec599d72401068cc108e6bd
Noah Young <noahpyoung@gmail.com>
2013-07-26 11:49:40 -0700

    mult remapping test works

--- mdp/test/test_theano.py
+++ mdp/test/test_theano.py
@@ -6,4 +6,4 @@ def test_mult_remapping():
     mdp.gputheano.activate_theano()
     assert type(mdp.utils.mult) == theano.compile.function_module.Function
     mdp.gputheano.deactivate_theano()
-    assert mdp.utils.mult == np.dot
+    assert mdp.utils.mult == numx.dot
