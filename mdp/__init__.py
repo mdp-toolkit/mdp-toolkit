fbdab2760c3165b2d65b85019a98358f12fcde69
Tiziano Zito <opossumnano@gmail.com>
2016-03-08 09:49:00 +0100

    update version number after release

--- mdp/__init__.py
+++ mdp/__init__.py
@@ -113,7 +113,7 @@ class MDPDeprecationWarning(DeprecationWarning, MDPWarning):
 
 from . import configuration
 
-__version__ = '3.4'
+__version__ = '3.5'
 __revision__ = configuration.get_git_revision()
 __authors__ = 'MDP Developers'
 __copyright__ = '(c) 2003-2016 mdp-toolkit-devel@lists.sourceforge.net'
