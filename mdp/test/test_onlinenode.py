ac1e63e5933f431d4e3e700a4d05d7d86248abc3
Varun Kompella <varunrajk@gmail.com>
2017-02-11 18:06:28 +0100

    ERF: Added a test function to check for the bug fix in commit a4910b987a4956c34c965ad6a44caab9811cc949.

--- mdp/test/test_onlinenode.py
+++ mdp/test/test_onlinenode.py
@@ -19,6 +19,17 @@ class BogusOnlineNodeMultiplePhases(mdp.OnlineNode):
     def _execute(self,x): return x + 1
     def _inverse(self,x): return x - 1
 
+class BogusOnlineNodeMultiplePhasesWithBlankTrainSeq(mdp.OnlineNode):
+    def _check_params(self, x):
+        if not hasattr(self, 'count'): self.count = None
+    def _get_train_seq(self):
+        seq = [(self._train, self._stop_training, self._execute)]*5
+        seq[-1] = (self._train, self._stop_training, lambda x: None)
+        return seq
+    def _train(self, x): self.count = x
+    def _execute(self,x): return x + 1
+    def _inverse(self,x): return x - 1
+
 def test_train_execute_iteration():
     node = BogusOnlineNode()
     assert (node.get_current_train_iteration() == 0)
@@ -67,6 +78,12 @@ def test_multiple_training_phases():
     node.train(inp)
     assert_array_equal(node.count, (node.get_remaining_train_phase()-1)*mdp.numx.ones(inp.shape))
 
+def test_batch_multiple_training_phases_train_iteration_with_blank_execute():
+    node = BogusOnlineNodeMultiplePhasesWithBlankTrainSeq()
+    node.set_training_type('batch')
+    inp = mdp.numx.zeros([2,5])
+    node.train(inp)
+    assert (node.get_current_train_iteration() == inp.shape[0])
 
 def test_node_add_online_node():
     flow = BogusOnlineNode() + BogusOnlineNode()
