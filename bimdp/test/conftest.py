e21cc78162fd76eb40c5b42ba8cc846ba2ffe872
user <user@rechenknecht.makesomethingup>
2019-01-20 02:38:55 +0100

    1. try to resolve pytest namespac deprecation

--- bimdp/test/conftest.py
+++ bimdp/test/conftest.py
@@ -13,11 +13,13 @@ To rule this out, please run the tests more than once.
 If you get reproducible failures please report a bug!
 """
 
+
 def pytest_configure(config):
     seed = config.getvalue("seed")
     # if seed was not set by the user, we set one now
     if seed is None or seed == ('NO', 'DEFAULT'):
         config.option.seed = int(mdp.numx_rand.randint(2**31-1))
+
 
 def pytest_unconfigure(config):
     # remove garbage created during tests
@@ -27,8 +29,10 @@ def pytest_unconfigure(config):
     # if pp was monkey-patched, remove any stale pp4mdp directories
     if hasattr(mdp.config, 'pp_monkeypatch_dirname'):
         monkey_dirs = os.path.join(mdp.config.pp_monkeypatch_dirname,
-                                   mdp.parallel.pp_support.TEMPDIR_PREFIX) 
-        [shutil.rmtree(d, ignore_errors=True) for d in glob.glob(monkey_dirs+'*')]
+                                   mdp.parallel.pp_support.TEMPDIR_PREFIX)
+        [shutil.rmtree(d, ignore_errors=True)
+         for d in glob.glob(monkey_dirs+'*')]
+
 
 def pytest_runtest_setup(item):
     # set random seed before running each test
@@ -37,15 +41,18 @@ def pytest_runtest_setup(item):
     # to run the whole test suite again
     mdp.numx_rand.seed(item.config.option.seed)
 
+
 def pytest_addoption(parser):
     """Add random seed option to py.test.
     """
     parser.addoption('--seed', dest='seed', type=int, action='store',
                      help='set random seed')
 
+
 def pytest_report_header(config):
     # report the random seed before and after running the tests
     return '%s\nRandom Seed: %d\n' % (mdp.config.info(), config.option.seed)
+
 
 def pytest_terminal_summary(terminalreporter):
     # add a note about error due to randomness only if an error or a failure
@@ -56,9 +63,3 @@ def pytest_terminal_summary(terminalreporter):
                                           t.config.option.seed))
     if 'failed' in t.stats or 'error' in t.stats:
         t.write_line(_err_str)
-
-def pytest_namespace():
-    # get temporary directory to put temporary files
-    # will be deleted at the end of the test run
-    dirname = tempfile.mkdtemp(suffix='.tmp', prefix='MDPtestdir_')
-    return dict(mdp_tempdirname=dirname)
