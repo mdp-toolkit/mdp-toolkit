053d5f9d6068f38adc068a358a6d4c8684e63571
NiMlr <nils.mueller-p43@rub.de>
2019-05-22 19:58:12 +0200

    FIX: test of Unevenly sampled SFA Node

--- mdp/test/test_UnevenlySampledSFANode.py
+++ mdp/test/test_UnevenlySampledSFANode.py
@@ -4,19 +4,25 @@ from mdp.test._tools import assert_array_almost_equal, decimal
 
 
 def test_UnevenlySampledSFANode1():
-    x = numx.random.random((10000,10))
-    dt = (numx.random.rand(x.shape[0]-1)-.5)*.2 + 1.
+    # generate data
+    x = numx.random.random((100000, 3))
+    # minimal noise on time increment
+    dt = (numx.random.rand(x.shape[0]-1)-.5)*0.01 + 1.
 
+    # initialize nodes
     sfa = SFANode()
     unsfa = UnevenlySampledSFANode()
 
+    # train sfa
     sfa.train(x)
     unsfa.train(x, dt)
 
+    # stop training and generate slow features
     sfa.stop_training()
     unsfa.stop_training()
 
-    y = sfa.execute(x, n=2)
-    uny = unsfa.execute(x, n=2)
+    if ((sfa.sf[0, 0] > 0) and (unsfa.sf[0, 0] < 0)) \
+            or ((sfa.sf[0, 0] < 0) and (unsfa.sf[0, 0] > 0)):
+        sfa.sf *= -1.
 
-    assert_array_almost_equal(y, uny, decimal-3)
+    assert_array_almost_equal(unsfa.sf, sfa.sf, decimal-6)
